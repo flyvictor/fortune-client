cce467ac6c493a380fd888c4906d0b10d8724a89
Dmitry Skrinnik <dmitry@skrinnik.com>
2014-12-09 21:52:08 +0200

    Most of the methods are ready

--- test/integration/fortune-client.js
+++ test/integration/fortune-client.js
@@ -277,201 +277,201 @@ module.exports = function(util){
       }).catch(function(err){ console.trace(err); });;
     });
 
-    //it("allows getting a document based on a query", function(done){
-      //client.getUsers({name: "Sweeney"}).then(function(data){
-        //data.users.length.should.be.equal(1);
-        //data.users[0].name.should.be.equal("Sweeney");
-        //done();
-      //});
-    //});
+    it("allows getting a document based on a query", function(done){
+      client.getUsers({name: "Sweeney"}).then(function(data){
+        data.users.length.should.be.equal(1);
+        data.users[0].name.should.be.equal("Sweeney");
+        done();
+      });
+    });
 
-    //it("allows getting a single document", function(done){
-      //var id = ids.users[0];
+    it("allows getting a single document", function(done){
+      var id = ids.users[0];
 
-      //client.getUser(id).then(function(data){
-        //data.users.length.should.be.equal(1);
-        //data.users[0].id.should.be.equal(id);
-        //done();
-      //});
-    //});
+      client.getUser(id).then(function(data){
+        data.users.length.should.be.equal(1);
+        data.users[0].id.should.be.equal(id);
+        done();
+      });
+    });
 
-    //it("allows getting a set of documents", function(done){
-      //client.getUsers([ids.users[0], ids.users[1]]).then(function(body){
-        //body.users.length.should.be.equal(2);
-        //done();
-      //});
-    //});
+    it("allows getting a set of documents", function(done){
+      client.getUsers([ids.users[0], ids.users[1]]).then(function(body){
+        body.users.length.should.be.equal(2);
+        done();
+      });
+    });
 
-    //it("allows creating documents", function(done){
-      //var user1 = {
-        //name: "Joe",
-        //email: "joe@abc.com"
-      //}, user2 = {
-        //name: "Phil",
-        //email: "phil@abc.com"
-      //};
+    it("allows creating documents", function(done){
+      var user1 = {
+        name: "Joe",
+        email: "joe@abc.com"
+      }, user2 = {
+        name: "Phil",
+        email: "phil@abc.com"
+      };
       
-      //client.createUsers([user1, user2]).then(function(data){
-        //data.users.length.should.be.equal(2);
-        //done();
-      //});
-    //});
+      client.createUsers([user1, user2]).then(function(data){
+        data.users.length.should.be.equal(2);
+        done();
+      });
+    });
 
-    //it("allows creating a single document", function(done){
-      //var user = {
-        //name: "Sam",
-        //email: "sam@abc.com"
-      //};
+    it("allows creating a single document", function(done){
+      var user = {
+        name: "Sam",
+        email: "sam@abc.com"
+      };
 
-      //client.createUser(user).then(function(data){
-        //data.users.length.should.be.equal(1);
-        //done();
-      //});
-    //});
+      client.createUser(user).then(function(data){
+        data.users.length.should.be.equal(1);
+        done();
+      });
+    });
 
-    //it("allows destroying a document", function(done){
-      //var count, id;
+    it("allows destroying a document", function(done){
+      var count, id;
       
-      //client.getUsers().then(function(data){
-        //count = data.users.length;
-        //count.should.be.above(0);
-        //return client.destroyUser(id = data.users[0].id);
-      //}).then(function(){
-        //return client.getUsers();
-      //}).then(function(data){
-        //data.users.length.should.be.equal(count - 1);
-        //_.contains(_.pluck(data.users, "id"), id).should.be.false;
-        //done();
-      //});
-    //});
+      client.getUsers().then(function(data){
+        count = data.users.length;
+        count.should.be.above(0);
+        return client.destroyUser(id = data.users[0].id);
+      }).then(function(){
+        return client.getUsers();
+      }).then(function(data){
+        data.users.length.should.be.equal(count - 1);
+        _.contains(_.pluck(data.users, "id"), id).should.be.false;
+        done();
+      });
+    });
 
-    //it("allows destroying a set of documents", function(done){
-      //var ids, count;
+    it.skip("allows destroying a set of documents", function(done){
+      var ids, count;
       
-      //client.getUsers().then(function(data){
-        //count = data.users.length;
-        //ids = [data.users[0].id,data.users[1].id];
+      client.getUsers().then(function(data){
+        count = data.users.length;
+        ids = [data.users[0].id,data.users[1].id];
 
-        //return client.destroyUsers(ids);
-      //}).then(function(){
-        //return client.getUsers();
-      //}).then(function(data){
-        //data.users.length.should.be.equal(count-2);
-        //_.contains(_.pluck(data.users, "id"), ids[0]).should.be.false;
-        //_.contains(_.pluck(data.users, "id"), ids[1]).should.be.false;
-        //done();
-      //});
-    //});
+        return client.destroyUsers(ids);
+      }).then(function(){
+        return client.getUsers();
+      }).then(function(data){
+        data.users.length.should.be.equal(count-2);
+        _.contains(_.pluck(data.users, "id"), ids[0]).should.be.false;
+        _.contains(_.pluck(data.users, "id"), ids[1]).should.be.false;
+        done();
+      });
+    });
 
-    //it("allows destroying a collection", function(done){
-      //client.destroyUsers().then(function(){
-        //return client.getUsers();
-      //}).then(function(data){
-        //data.users.length.should.be.equal(0);
-        //done();
-      //});
-    //});
+    it("allows destroying a collection", function(done){
+      client.destroyUsers().then(function(){
+        return client.getUsers();
+      }).then(function(data){
+        data.users.length.should.be.equal(0);
+        done();
+      });
+    });
 
 
-    //it("allows replacing a document", function(done){
-      //var user;
+    it("allows replacing a document", function(done){
+      var user;
       
-      //client.getUser(ids.users[0]).then(function(data){
-        //user = data.users[0];
-        //return client.replaceUser(user.id, _.extend({}, user, {name: "1234"}));
-      //}).then(function(data){
-        //return client.getUser(ids.users[0]);
-      //}).then(function(data){ 
-        //data.users[0].name.should.be.equal("1234");
-        //done();
-      //}).catch(function(err){ console.trace(err); });
-    //});
+      client.getUser(ids.users[0]).then(function(data){
+        user = data.users[0];
+        return client.replaceUser(user.id, _.extend({}, user, {name: "1234"}));
+      }).then(function(data){
+        return client.getUser(ids.users[0]);
+      }).then(function(data){ 
+        data.users[0].name.should.be.equal("1234");
+        done();
+      }).catch(function(err){ console.trace(err); });
+    });
 
-    //it("allows updating a document", function(done){
-      //client.updateUser(ids.users[0], [{
-        //op: "add",
-        //path: "/users/0/instruments/-",
-        //value: ids.instruments[0]
-      //}]).then(function(data){
-        //data.users[0].links.instruments.length.should.equal(1);
-        //data.users[0].links.instruments[0].should.equal(ids.instruments[0]);
-        //done();
-      //}).catch(function(err){ console.trace(err); });;
-    //});
+    it("allows updating a document", function(done){
+      client.updateUser(ids.users[0], [{
+        op: "add",
+        path: "/users/0/instruments/-",
+        value: ids.instruments[0]
+      }]).then(function(data){
+        data.users[0].links.instruments.length.should.equal(1);
+        data.users[0].links.instruments[0].should.equal(ids.instruments[0]);
+        done();
+      }).catch(function(err){ console.trace(err); });;
+    });
 
-    //it("returns JSON-compatible objects", function(done){
-      //client.getUsers().then(function(data){
-        //JSON.stringify(data).should.be.ok;
-        //done();
-      //});
-    //});
+    it("returns JSON-compatible objects", function(done){
+      client.getUsers().then(function(data){
+        JSON.stringify(data).should.be.ok;
+        done();
+      });
+    });
 
-    //it("camelcases dashed resource names", function(done){
-      //client.getNaNaNaNas().then(function(data){
-        //data["na-na-na-nas"].length.should.be.above(0);
-        //done();
-      //});
-    //});
+    it("camelcases dashed resource names", function(done){
+      client.getNaNaNaNas().then(function(data){
+        data["na-na-na-nas"].length.should.be.above(0);
+        done();
+      });
+    });
 
-    //it("supports the light syntax for fields", function(done){
-      //client.getUsers(null, {fields: "name"}).then(function(data){
-        //var fields = _.keys(data.users[0]);
+    it("supports the light syntax for fields", function(done){
+      client.getUsers(null, {fields: "name"}).then(function(data){
+        var fields = _.keys(data.users[0]);
         
-        //fields.length.should.be.equal(2); // id is included regardless of fields
-        //_.contains(fields, "name").should.be.true;
-        //_.contains(fields, "id").should.be.true;
+        fields.length.should.be.equal(2); // id is included regardless of fields
+        _.contains(fields, "name").should.be.true;
+        _.contains(fields, "id").should.be.true;
         
-        //return client.getUsers(null, {fields: ["name", "email"]});
-      //}).then(function(data){
-        //var fields = _.keys(data.users[0]);
+        return client.getUsers(null, {fields: ["name", "email"]});
+      }).then(function(data){
+        var fields = _.keys(data.users[0]);
         
-        //fields.length.should.be.equal(3);
-        //_.contains(fields, "name").should.be.true;
-        //_.contains(fields, "id").should.be.true;
-        //_.contains(fields, "email").should.be.true;          
-        //done();
-      //});
-    //});
+        fields.length.should.be.equal(3);
+        _.contains(fields, "name").should.be.true;
+        _.contains(fields, "id").should.be.true;
+        _.contains(fields, "email").should.be.true;          
+        done();
+      });
+    });
 
-    //it("passes the parent request context option to onRequest", function(done){
-      //var parent = { parentRequest: "is me" };
+    it("passes the parent request context option to onRequest", function(done){
+      var parent = { parentRequest: "is me" };
       
-      //client.onRequest(function(config){
-        //config.parentRequest.should.be.equal(parent);
-        //done();
-      //});
+      client.onRequest(function(config){
+        config.parentRequest.should.be.equal(parent);
+        done();
+      });
 
-      //client.getUsers(null, {parentRequest: parent});
-    //});
+      client.getUsers(null, {parentRequest: parent});
+    });
 
-    //it("passes the parent request context option to onResponse", function(done){
-      //var parent = { parentRequest: "is me" };
+    it("passes the parent request context option to onResponse", function(done){
+      var parent = { parentRequest: "is me" };
       
-      //client.onResponse(function(config){
-        //config.parentRequest.should.be.equal(parent);
-        //done();
-      //});
+      client.onResponse(function(config){
+        config.parentRequest.should.be.equal(parent);
+        done();
+      });
 
-      //client.getUsers(null, {parentRequest: parent});
-    //});
+      client.getUsers(null, {parentRequest: parent});
+    });
 
 
-    //it("supports alternative programmatic syntax", function(done){
-      //client.getUsers().then(function(data){
-        //client.get("users").then(function(data2){
-          //data2.should.be.eql(data);
-        //});
-      //}).then(function(){
-        //client.getBands().then(function(data){
-          //client.get("bands").then(function(data2){
-            //data2.should.be.eql(data);
-            //done();
-          //});
-        //});
-      //});
-    //});
+    it("supports alternative programmatic syntax", function(done){
+      client.getUsers().then(function(data){
+        client.get("users").then(function(data2){
+          data2.should.be.eql(data);
+        });
+      }).then(function(){
+        client.getBands().then(function(data){
+          client.get("bands").then(function(data2){
+            data2.should.be.eql(data);
+            done();
+          });
+        });
+      });
+    });
 
-    //util.requireSpecs(__dirname, ["compound-documents"]);
+    util.requireSpecs(__dirname, ["compound-documents"]);
   });
 };
 
